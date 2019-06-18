daa5f8942ffae634d6b2244e759c320e4118cd5d
ZohoPageSense <46103894+ZohoPageSense@users.noreply.github.com>
2019-06-18 13:41:48 +0530

    Update sw_ps.js

--- dev/push/sw_ps.js
+++ dev/push/sw_ps.js
@@ -1,1 +1,1 @@
-importScripts('https://rajesh-1093.csez.zohocorpin.com:8443/js/sw_ps.js?_='+Math.floor(Date.now()/86400000))
+importScripts('https://rajesh-1093.csez.zohocorpin.com:8443/js/sw_ps.js?_='+Math.floor(Date.now()));
